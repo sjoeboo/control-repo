7c5a67899a087cabd91da24e6d1dc9c264191b7e
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2016-05-18 10:21:00 -0400

    setup a default rule

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,8 +2,10 @@ class profile::base {
   class { '::ntp': }
   class { '::consul': }
   class { '::docker': }
-  
+
   $docker_containers = hiera('docker_containers',{})
   create_resources(docker::run,$docker_containers)
-}
 
+  $firewall_rules = hiera_hash('firewall_rules',{})
+  create_resources(firewall,$firewall_rules)
+}
